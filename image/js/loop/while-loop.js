a2e2ebde9ded888bb427bdded1aeca2dc1325c4c
tanjil-hosain <tanjilh85@gmail.com>
2026-02-26 23:58:31 +0600

    add

---
+++ image/js/loop/while-loop.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+//While loop
+//eti totokkhon cole jotokkhon conditon mittha hoy--------------->
+
+let a = 10;
+while(a<=20){
+    console.log(a);
+    a++;
+}
+
+let i = Number(prompt('Enter your value'));
+while(i<=6){
+    console.log(i);
+    i++;
+}
